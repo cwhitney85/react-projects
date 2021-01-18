33b6754c9d783a98102f36b4d8471a20d50eda56
Colin Whitney <colin.whitney@gmail.com>
2021-01-18 14:26:42 -0500

    completed tour exercise

--- 2-tours/setup/src/Tours.js
+++ 2-tours/setup/src/Tours.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours, removeTour}) => {
+  return (
+    <section>
+      <div className='title'>
+        <h2>our tours</h2>
+        <div className='underline'></div>
+        <div>
+          {tours.map(tour => {
+            // Spread object allows to pass all the values as props
+            return <Tour key={tour.id} {...tour} removeTour={removeTour}></Tour>
+          })}
+        </div>
+      </div>
+    </section>
+  )
 };
 
 export default Tours;
