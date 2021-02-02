80ca1229caf9c6123c381c03951c55359013d4af
Colin Whitney <colin.whitney@gmail.com>
2021-02-02 17:42:49 -0500

    edit functionality working

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -21,6 +21,18 @@ function App() {
     }
     else if (name && isEditing) {
       // deal with edit
+      setList(
+        list.map((item)=> {
+          if (item.id === editID) {
+            return {...item, title: name}
+          }
+          return item
+        })
+      )
+      setName('');
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, 'success', 'value changed')
     }
     else {
       // show alert
@@ -45,6 +57,13 @@ function App() {
     setList(list.filter((item)=> item.id !== id))
   }
 
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title)
+  }
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
@@ -65,7 +84,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} removeItem={removeItem} />
+          <List items={list} removeItem={removeItem} editItem={editItem}/>
           <button className="clear-btn" onClick={clearList}>clear items</button>
         </div>
       )}
