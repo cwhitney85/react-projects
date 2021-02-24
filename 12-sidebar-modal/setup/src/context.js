4fcca66fd837d5fdf8edbda5071314cbf68268a7
Colin Whitney <colin.whitney@gmail.com>
2021-02-24 16:29:02 -0500

    set up context provider, wrapping app, set up custom hook

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,16 @@
 import React, { useState, useContext } from 'react'
+
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  return (
+    <AppContext.Provider value="heloo">{children}</AppContext.Provider>
+  )
+}
+// custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
